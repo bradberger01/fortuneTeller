536d51a5c0c9a0e3ea9ff3b0cda67ca4063a13f2
bradberger01 <bradberger@hotmail.com>
2017-08-23 21:05:55 -0400

    working on part 1 of fortuneTeller

--- fortuneTeller/Program.cs
+++ fortuneTeller/Program.cs
@@ -10,6 +10,32 @@ namespace fortuneTeller
     {
         static void Main(string[] args)
         {
+            //Computer asks for firstName, lastName, age, birthMonth (as in int), favColor (by ROYGBIV - if usser doesn't know ROYGBIV, offer "help" to explain, numOfSibs
+
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + firstName +  ". What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Okay. Now enter your birth month (by number).");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            //Make a formula that gives equates a month to the entered number. Make sure it will work if someone enters 1 or 01.
+            // if(birthMonth == 1 || birthMonth == 01) use this later on for part 2
+
+            Console.WriteLine("Using ROYGBIV, enter your favorite color. If you don't know ROYGBIV type in \r\n\"Help\"");
+            string favColor = Console.ReadLine();
+
+            if (favColor == "Help")
+            {
+                Console.WriteLine("Red\r\nOrange\r\nYellow\r\nGreen\r\nBlue\r\nIndigo\r\nViolet")
+            }
+
+            
         }
     }
 }
